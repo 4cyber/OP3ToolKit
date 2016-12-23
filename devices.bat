9a5229e8c9b9d4f71c99df2477152eec9a8db250
4cyber <andrea.belli.96@gmail.com>
2016-12-23 12:00:15 +0100

    Update devices.bat

--- devices.bat
+++ devices.bat
@@ -1,9 +1,14 @@
-
 @echo off
-color f0
 title Detector
 mode con:cols=35 lines=9
+color f0
 :AGAIN
+if exist verde.txt color 0a
+if exist giallo.txt color 0e
+if exist bianco.txt color f0
+del verde.txt >nul
+del giallo.txt >nul
+del bianco.txt >nul
 cls
 echo DEVICE DETECTOR
 echo.
